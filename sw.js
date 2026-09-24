919c1eb140561c8c7eadbfdf4a03dbda43e50ded
quicknoteshop-hub <quicknoteshop@gmail.com>
2026-09-24 02:10:49 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'qs-v1';
+const STATIC = ['./','./index.html','./manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+});
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
