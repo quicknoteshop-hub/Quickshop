e57ef8d89f912e11d79c51ed7abac6594e73f063
quicknoteshop-hub <quicknoteshop@gmail.com>
2026-09-24 02:33:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'qs-v1';
+const CACHE = 'qs-v2';
 const STATIC = ['./','./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
